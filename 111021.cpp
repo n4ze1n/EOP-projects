be568a98aa4b24152f5606916800a78ed75ec996
n4ze1n <81848463+n4ze1n@users.noreply.github.com>
2021-10-12 21:11:15 +0800

    111121

---
+++ 111021.cpp
@@ -0,0 +1,31 @@
+//data type 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int x = 10;
+	float a;
+	double b = 10.21;
+	char c = 'S';
+	string s = "hannan zainudin";
+	bool  d = true;
+
+	
+	cout << x << endl;
+	//cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << s << endl;
+	cout << d << endl;
+	cin << "value:" << a << endl;
+	return 0;
+	
+}
+
+
+
+
+
+
