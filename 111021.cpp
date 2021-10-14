43faf829355c1ac3585be1be66cf8e9e0eb34c38
n4ze1n <81848463+n4ze1n@users.noreply.github.com>
2021-10-14 09:54:27 +0800

    updated

--- 111021.cpp
+++ 111021.cpp
@@ -5,21 +5,49 @@ using namespace std;
 int main()
 {
 
-	int x = 10;
+/*	int x = 10;
 	float a;
 	double b = 10.21;
 	char c = 'S';
 	string s = "hannan zainudin";
 	bool  d = true;
+	*/
+	string name;
 
 	
-	cout << x << endl;
-	//cout << a << endl;
+	/*cout << x << endl;
+	cout << a << endl;
 	cout << b << endl;
 	cout << c << endl;
 	cout << s << endl;
 	cout << d << endl;
-	cin << "value:" << a << endl;
+	cout << "enter your number: ";
+	cin >> a;
+	cout << "the number is:" <<a; 
+	*/
+	getline(cin, name);
+	cout << "hello " << name;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 	
 }
